3c0529235a57f522ef0a01ab0e34050eb43fa161
Kamya Jain <72403163+KamyaJain@users.noreply.github.com>
2024-05-18 08:39:29 +0000

    operations done!

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)  ==>> -3
+
+// console.log(2+2)   ==>> 4
+// console.log(2-2)   ==>> 0
+// console.log(2*2)   ==>> 4
+// console.log(2**3)  ==>> 8
+// console.log(2/2)   ==>> 1
+// console.log(2%3)   ==>> 2
+
+let str1 = "hello"
+let str2 = " kamya"
+let str3 = str1 + str2
+// console.log(str3)   ==>> hello kamya
+
+// console.log("1" + 2)   ==>> 12
+// console.log(1 + "2")   ==>> 12
+
+// console.log(1 + 2 + "2")  ==>> 32
+// console.log("1" + 2 + 2)   ==>> 122
+
+
+// not a good practice
+
+console.log(+true) // 1 its changing it to number as we are adding 1 to it but the operation will not be performed and it return the value of true i.e. 1
+
+console.log(+"") // 0 same reason as stated above it will print the value of "" which is false i.e. 0
+
